df12e14769c4e5ca554f095d49dd84a53f58f04e
Danang Rahmatullah <danang.rahmatullah@alltrons.com>
2024-03-08 17:48:30 +0100

    Fix address printing

--- src/ghostscript_dart.c
+++ src/ghostscript_dart.c
@@ -24,7 +24,7 @@ FFI_PLUGIN_EXPORT int gsdart_init_with_args(int argc, char **argv)
   printf("argc=%i\n", argc);
   for (int i = 0; i < argc; ++i)
   {
-    printf("String %i @ %p: %s\n", i, &argv[i], argv[i]);
+    printf("String %i @ %p: %s\n", i, &argv[i][0], argv[i]);
   }
   return gsapi_init_with_args(gsinst, argc, argv);
 }
